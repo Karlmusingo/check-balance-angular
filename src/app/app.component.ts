ae7d1d6c505bdf5fd7254d8cc2d4915bf9b1af4c
karlmusingo <karlemusingo@gmail.com>
2021-07-23 22:20:03 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'check-balance-angular';
+  title = 'check balance';
+  transactions = [
+    {
+      _id: '3e24241143144312',
+      description: 'A fake description',
+      amount: 20,
+      transactionType: 'DEBIT',
+    },
+    {
+      _id: '3e24241135444312',
+      description: 'A fake description',
+      amount: 25,
+      transactionType: 'CREDIT',
+    },
+  ];
 }
